a856374b70ad15b6e01a18d854920f56dc65e108
oheydrew <oheydrew.codes@gmail.com>
2017-06-11 18:58:10 +1000

    multiplesOf3 array, loops begun

--- js/euler_problem1.js
+++ js/euler_problem1.js
@@ -1,3 +1,12 @@
+// Messing with solution display within index.html
 document.getElementById('solutionText').innerHTML = 'test';
 
-console.log('Fuuuuck');
+var multipleOf3 = [];
+
+for (i = 0; i <= 1000; i++) {
+    if (i%3 === 0) {
+      multipleOf3.push(i);
+    }
+}
+
+console.log(multipleOf3);
