4a43c31c18da23c6e6f4160877c99ae446db4f42
oheydrew <oheydrew.codes@gmail.com>
2017-06-11 19:33:00 +1000

    sum array, console logging solution

--- js/euler_problem1.js
+++ js/euler_problem1.js
@@ -1,12 +1,20 @@
 // Messing with solution display within index.html
 document.getElementById('solutionText').innerHTML = 'test';
 
-var multipleOf3 = [];
+var multipleArray = [];
 
 for (i = 0; i <= 1000; i++) {
-    if (i%3 === 0) {
-      multipleOf3.push(i);
+    if (i%3 === 0 || i%5 === 0) {
+      multipleArray.push(i);
     }
 }
 
-console.log(multipleOf3);
+/*  use .reduce function to sum the array.
+    .reduce takes 4 arguments, accumulator, currentValue, currentIndex, and array
+    used the default names for my arguments for clarity
+*/
+var summedMultiples = multipleArray.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+});
+
+console.log(summedMultiples);
